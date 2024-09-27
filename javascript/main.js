c462af3f2db6998f96017976569639f851f1cba0
Niraj Kumar Singh <91699259+nirajksh@users.noreply.github.com>
2024-09-27 12:00:21 +0530

    new solved problem

---
+++ javascript/main.js
@@ -0,0 +1,58 @@
+console.log("hello javascript ");
+// sum of two number
+// function sumOfTwoNumber(a, b) {
+//   return a + b;
+// }
+
+// const res = sumOfTwoNumber(56, 8);
+// console.log(res);
+
+//max number of array
+
+// function maxNumber(arr) {
+//   return Math.min(...arr);
+// }
+// const arr = [8, 5, 7, 9, 12, 1, 88];
+// const res = maxNumber(arr);
+// console.log(res);
+
+// function maxNumber(arr) {
+//   let res = [];
+//   for (let i = 0; i <= arr.length; i++) {
+//     res.push(arr[i]);
+//   }
+//   return res;
+// }
+// const arr = [8, 5, 7, 9, 12, 1, 88];
+// const res = maxNumber(arr);
+// console.log(res);
+
+// given string palindrome or not
+
+// function checkPalindrome(str) {
+//   return str === str.split("").reverse().join("");
+// }
+
+// const res = checkPalindrome("abcddcba");
+// console.log(res);
+
+// reverse the string
+
+// function reverseString(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// const res = reverseString("abhishek");
+// console.log(res);
+
+//another method try it     another way
+
+// function reverse(str) {
+//   for (let i = 0; i < str.length; i++) {
+//     return str[i];
+//   }
+// }
+// const res = reverse("abishek ");
+// console.log(res);
+
+//
